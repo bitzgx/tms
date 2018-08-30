862121ab1724c47e130409a0686cc505586e81b6
gondiski <enochotieno@rocketmail.com>
2018-08-30 17:55:48 +0300

    Added controllers

--- src/main/java/com/terminal/tms/controller/TmsController.java
+++ src/main/java/com/terminal/tms/controller/TmsController.java
@@ -1,10 +1,25 @@
 package com.terminal.tms.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.terminal.tms.model.Tms;
+import com.terminal.tms.repository.TmsRepository;
 
 @RestController
 @RequestMapping("/")
 public class TmsController {
+	@Autowired
+	private TmsRepository tmsRepository;
+	
+	@RequestMapping("/")
+	public List<Tms> getAllTms(){
+		return tmsRepository.findAll();
+	}
+	
+	@RequestMapping("/{id}")
 	
 }
