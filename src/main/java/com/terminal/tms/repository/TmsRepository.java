bde115e2b87d95ee3efbf08970ded0208752c70e
gondiski <enochotieno@rocketmail.com>
2018-09-03 14:13:21 +0300

    This is a RESTFUL API implementation of Terminal Management System with SpringBoot

--- src/main/java/com/terminal/tms/repository/TmsRepository.java
+++ src/main/java/com/terminal/tms/repository/TmsRepository.java
@@ -1,6 +1,5 @@
 package com.terminal.tms.repository;
 
-//import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +7,5 @@ import com.terminal.tms.model.Tms;
 
 @Repository
 public interface TmsRepository extends JpaRepository <Tms, Long> {
-//	List<Tms> findByTmsId(Long tmsId);
+
 }
